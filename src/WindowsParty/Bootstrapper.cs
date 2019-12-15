3c5b1d40232fd9e86684ffc1c7545e4133e1c216
specown <lukasaleksaa@gmail.com>
2019-12-15 18:47:12 +0000

    added configuration for bootstrapper

--- src/WindowsParty/Bootstrapper.cs
+++ src/WindowsParty/Bootstrapper.cs
@@ -1,4 +1,6 @@
 ﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
 using System.Windows;
 using WindowsParty.ViewModels;
 
@@ -6,14 +8,44 @@ namespace WindowsParty
 {
     public class Bootstrapper : BootstrapperBase
     {
+        private SimpleContainer _simpleContainer;
+
         public Bootstrapper()
         {
             Initialize();
         }
 
+        protected override void Configure()
+        {
+            _simpleContainer = new SimpleContainer();
+
+            _simpleContainer.Singleton<IWindowManager, WindowManager>()
+                .Singleton<IEventAggregator, EventAggregator>();
+
+
+            _simpleContainer.PerRequest<ShellViewModel>()
+                .PerRequest<LoginViewModel>()
+                .PerRequest<ServerListViewModel>();
+        }
+
         protected override void OnStartup(object sender, StartupEventArgs e)
         {
             DisplayRootViewFor<ShellViewModel>();
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            return _simpleContainer.GetInstance(service, key);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _simpleContainer.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            _simpleContainer.BuildUp(instance);
         }
     }
 }
