7e0e247af0251097604461c8487b016500bd68b5
specown <lukasaleksaa@gmail.com>
2019-12-15 18:52:31 +0000

    Created base of login view model to get typed in username and password

--- src/WindowsParty/ViewModels/LoginViewModel.cs
+++ src/WindowsParty/ViewModels/LoginViewModel.cs
@@ -1,7 +1,39 @@
-﻿
+﻿using Caliburn.Micro;
+using System;
+
 namespace WindowsParty.ViewModels
 {
-    public class LoginViewModel
+    public class LoginViewModel : Screen
     {
+        private IEventAggregator _eventAggregator;
+        private string _username;
+        private string _password;
+
+        public LoginViewModel(IEventAggregator eventAggregator)
+        {
+            _eventAggregator = eventAggregator ?? throw new ArgumentNullException(nameof(eventAggregator));
+        }
+
+        public string Username
+        {
+            get => _username;
+            set
+            {
+                _username = value;
+                NotifyOfPropertyChange(() => Username);
+            }
+        }
+
+        public string Password
+        {
+            get => _password;
+            set
+            {
+                _password = value;
+                NotifyOfPropertyChange(() => Password);
+            }
+        }
+
+        
     }
 }
