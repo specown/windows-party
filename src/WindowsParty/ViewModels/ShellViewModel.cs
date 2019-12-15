9a6e48a5aa09016e9c7ae903a870bd7500dabb71
specown <lukasaleksaa@gmail.com>
2019-12-15 18:46:30 +0000

    Main structure of shell view model

--- src/WindowsParty/ViewModels/ShellViewModel.cs
+++ src/WindowsParty/ViewModels/ShellViewModel.cs
@@ -1,11 +1,10 @@
-﻿
-
-using Caliburn.Micro;
+﻿using Caliburn.Micro;
 using System;
+using WindowsParty.Events;
 
 namespace WindowsParty.ViewModels
 {
-    public class ShellViewModel : Screen
+    public class ShellViewModel : Conductor<object>, IHandle<EventModel>
     {
         IEventAggregator _eventAggregator;
         LoginViewModel _loginViewModel;
@@ -16,6 +15,31 @@ namespace WindowsParty.ViewModels
             _eventAggregator = eventAggregator ?? throw new ArgumentNullException(nameof(eventAggregator));
             _loginViewModel = loginViewModel ?? throw new ArgumentNullException(nameof(loginViewModel));
             _serverListViewModel = serverListViewModel ?? throw new ArgumentNullException(nameof(serverListViewModel));
+
+            _eventAggregator.Subscribe(this);
+        }
+
+        protected override void OnInitialize()
+        {
+            base.OnInitialize();
+            ActivateItem(_loginViewModel);
+        }
+
+        public void Handle(EventModel eventMessage)
+        {
+            switch (eventMessage.Status)
+            {
+                case Status.Signin:
+                    ActivateItem(_serverListViewModel);
+                    break;
+
+                case Status.Logout:
+                    ActivateItem(_loginViewModel);
+                    break;
+
+                default:
+                    break;
+            }
         }
     }
 }
