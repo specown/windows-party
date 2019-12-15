e0c0d2c382a3c21266f45d9a14c2b9e2c1e9e24c
specown <lukasaleksaa@gmail.com>
2019-12-15 00:01:14 +0000

    Initial structure of MVMM

---
+++ src/WindowsParty/ViewModels/LoginViewModel.cs
@@ -0,0 +1,7 @@
+﻿
+namespace WindowsParty.ViewModels
+{
+    public class LoginViewModel
+    {
+    }
+}
---
+++ src/WindowsParty/ViewModels/ShellViewModel.cs
@@ -0,0 +1,21 @@
+﻿
+
+using Caliburn.Micro;
+using System;
+
+namespace WindowsParty.ViewModels
+{
+    public class ShellViewModel : Screen
+    {
+        IEventAggregator _eventAggregator;
+        LoginViewModel _loginViewModel;
+        ServerListViewModel _serverListViewModel;
+
+        public ShellViewModel(IEventAggregator eventAggregator, LoginViewModel loginViewModel, ServerListViewModel serverListViewModel)
+        {
+            _eventAggregator = eventAggregator ?? throw new ArgumentNullException(nameof(eventAggregator));
+            _loginViewModel = loginViewModel ?? throw new ArgumentNullException(nameof(loginViewModel));
+            _serverListViewModel = serverListViewModel ?? throw new ArgumentNullException(nameof(serverListViewModel));
+        }
+    }
+}
