802982bd4dee90b2aec1614a63073b7254d46e06
specown <lukasaleksaa@gmail.com>
2019-12-17 21:18:21 +0000

    Added interfaces for helpers

---
+++ src/WindowsParty/Interfaces/IAuthenticationHelper.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using WindowsParty.Models;
+
+namespace WindowsParty.Interfaces
+{
+    public interface IAuthenticationHelper
+    {
+        Task AuthenticateUser(UserModel userModel);
+    }
+}
