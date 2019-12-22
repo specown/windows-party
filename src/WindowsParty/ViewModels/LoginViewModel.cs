a22ac8e8ae5914db8c6c195d18069abe29907d23
specown <lukasaleksaa@gmail.com>
2019-12-22 01:37:02 +0000

    added error message and tidied up the code

--- src/WindowsParty/ViewModels/LoginViewModel.cs
+++ src/WindowsParty/ViewModels/LoginViewModel.cs
@@ -11,11 +11,11 @@ namespace WindowsParty.ViewModels
     {
         private IEventAggregator _eventAggregator;
         private IAuthenticationHelper _authenticationHelper;
-        private string _username;
-        private string _password;
+        private string _username, _password, _errorMessage;
 
         public LoginViewModel(IEventAggregator eventAggregator, IAuthenticationHelper authenticationHelper)
         {
+            //Initialize interfaces
             _eventAggregator = eventAggregator ?? throw new ArgumentNullException(nameof(eventAggregator));
             _authenticationHelper = authenticationHelper ?? throw new ArgumentNullException(nameof(authenticationHelper));
         }
@@ -26,6 +26,7 @@ namespace WindowsParty.ViewModels
             set
             {
                 _username = value;
+                //If property changed - notify dependants
                 NotifyOfPropertyChange(() => Username);
             }
         }
@@ -40,20 +41,62 @@ namespace WindowsParty.ViewModels
             }
         }
 
+        public bool ErrorDisplayed
+        {
+            get => ErrorMessage?.Length > 0 ? true : false;
+            set
+            {
+                NotifyOfPropertyChange(() => ErrorDisplayed);
+            }
+        }
+
+        public string ErrorMessage
+        {
+            get => _errorMessage;
+            set
+            {
+                _errorMessage = value;
+                NotifyOfPropertyChange(() => ErrorDisplayed);
+                NotifyOfPropertyChange(() => ErrorMessage);
+            }
+        }
+
+        public AuthModel AuthModel { get; private set; }
+
         public async Task AuthUser()
         {
             try
             {
-                UserModel userModel = new UserModel(Username, Password);
-                await _authenticationHelper.AuthenticateUser(userModel);
+                //Check if input is valid
+                if(validateInput())
+                {
+                    //Input is valid - create a user model object
+                    UserModel userModel = new UserModel(Username, Password);
 
-                _eventAggregator.PublishOnUIThread(new EventModel(Status.Signin));
+                    var authModel = await _authenticationHelper.AuthenticateUser(userModel);
+
+                    if(!string.IsNullOrEmpty(authModel.AuthToken))
+                    {
+                        AuthModel = authModel;
+                        _eventAggregator.PublishOnUIThread(new EventModel(Status.Signin));
+                    }                    
+                }
+                else
+                {
+                    ErrorMessage = "Invalid input";
+                }
             }
-            catch (Exception ex)
+            catch (TaskCanceledException ex)
             {
-                
+                LogManager.GetLog(this.GetType()).Info("Authorization was cancelled: {0}", ex);
             }
+        }
 
+        private bool validateInput()
+        {
+            //Check if the user has typed anything in
+            //If more details would've been given - could have used regex to filter out incorrect formats
+            return Username?.Length > 0 && Password?.Length > 0 ? true : false;
         }
     }
 }
