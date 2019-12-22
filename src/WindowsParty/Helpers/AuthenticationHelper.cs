82e9d71eb6990455e642c42b32e01d509995a75c
specown <lukasaleksaa@gmail.com>
2019-12-22 01:35:37 +0000

    added exception handling

--- src/WindowsParty/Helpers/AuthenticationHelper.cs
+++ src/WindowsParty/Helpers/AuthenticationHelper.cs
@@ -7,23 +7,45 @@ namespace WindowsParty.Helpers
 {
     public class AuthenticationHelper : IAuthenticationHelper
     {
+        private AuthModel _authModel;
         private IWebTasks _webTasks;
+
+        public AuthModel AuthModel
+        {
+            get { return _authModel; }
+            private set { _authModel = value; }
+        }
 
         public AuthenticationHelper(IWebTasks webTasks)
         {
             _webTasks = webTasks ?? throw new ArgumentNullException(nameof(webTasks));
         }
 
-        public async Task AuthenticateUser(UserModel userModel)
+        public async Task<AuthModel> AuthenticateUser(UserModel userModel)
         {
             try
             {
-                AuthModel authResponse = await _webTasks.AuthenticateUser(userModel);
+                var authResponse = await _webTasks.AuthenticateUser(userModel);
+
+                //Check if the returned authentication token is not empty
+                if (!String.IsNullOrWhiteSpace(authResponse.AuthToken))
+                {
+                    return AuthModel = authResponse;
+                }
+                else
+                {
+                    throw new Exception("Retrieved empty authentication token");
+                }
             }
             catch (Exception ex)
             {
-
+                throw new Exception("Failed calling authentication service");
             }
+        }
+
+        public void LogOut()
+        {
+            AuthModel = null;
         }
     }
 }
